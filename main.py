229a32eaec176fdedcb668c1f7a01f472e9f7c10
Richard Povea <richardpovea@gmail.com>
2026-03-09 19:03:25 -0300

    Validaciones con try except

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from geo import (
     flat_margin, percentage_margin
     )
 from state import State, Event, machine, FileType, get_file_type
+from errors import ColumnNotFoundError, DirectoryNotFoundError, NotADirectoryError, PathError
 
-from typing import Literal
+from typing import Literal, Self
 from pathlib import Path
 from numpy import pi, ndarray
 from datetime import datetime
@@ -102,6 +103,7 @@ def build_sqm_defined_ligths(
     ligths: GeoDataFrame,
     names: dict[str, str]=DEFAULT_ATTRIBUTE_NAMES):
     points = ligths.geometry
+    print(points.iloc[0])
     print(f"Calculando SQM con {points.shape[0]} fuentes lumínicas")
     light_flux = (
         ligths[names["power"]] * ligths[names["eficiency"]]
@@ -120,11 +122,20 @@ def filter_geodf(
     return geodf.loc[geodf[column]==equals_to].copy()
 
 def ask_for_input():
-    points_path = input("Ingrese la ruta del archivo de puntos (shapefile o geopackage): ")
+    points_path = input("Ingrese la ruta del archivo de puntos (shapefile o geopackage): \n")
     points_path = Path(points_path)
     if not points_path.exists():
         raise FileNotFoundError(f"No se encontró el archivo en {points_path}")
     return points_path
+
+def ask_for_output_dir():
+    output_dir = input()
+    output_dir = Path(output_dir)
+    if not output_dir.exists():
+        raise DirectoryNotFoundError(output_dir)
+    if not output_dir.is_dir():
+        raise 
+    return output_dir
 
 class GeoFile:
     def __init__(self, file_path: Path):
@@ -150,17 +161,26 @@ class GeoFile:
     @property
     @lru_cache
     def columns(self):
-        return self.geodata.columns
+        cols = self.geodata.columns.to_list()
+        cols.remove("geometry")
+        return {idx: col for idx, col in enumerate(cols)}
 
-    def filter_data(self, column: str, equals_to: str):
-        if column not in self.columns:
-            raise ValueError(f"No se encontró la columna {column} en el archivo {self.file_path.stem}")
+    def filter_data(self, column: str, equals_to: str) -> Self:
+        self._validate_column(column)
         self._geo_data = filter_geodf(
             geodf=self.geodata,
             column=column,
             equals_to=equals_to
         )
         return self
+    
+    def valids_to_filter_values(self, column: str):
+        self._validate_column(column)
+        return self.geodata[column].unique()
+    
+    def _validate_column(self, column: str):
+        if column not in self.columns.values():
+            raise ColumnNotFoundError(column, self.file_path)
         
 class GPKGFile(GeoFile):
     def __init__(self, file_path: Path):
@@ -171,74 +191,118 @@ class GPKGFile(GeoFile):
     def layers(self):
         return list_layers(self.file_path)
     
-    def set_layer(self, layer_name: str):
-        if layer_name not in self.layers:
-            raise ValueError(f"No se encontró la capa {layer_name} en el archivo {self.file_path}")
-        self.layer = layer_name
+    def set_layer(self, layer_index: str):
+        layer = int(layer_index)
+        if layer < 0 or layer >= len(self.layers):
+            raise ValueError(f"Índice de capa inválido. Ingrese un número entre 0 y {len(self.layers)-1}")
+        layer = self.layers.iloc[int(layer_index)]["name"]
+        self.layer = layer
+        return self
 
 class SHPFile(GeoFile):
     def __init__(self, file_path: Path):
         super().__init__(file_path)
         
 
-def get_file(file_path: Path) -> GeoFile:
+def get_file(file_path: Path) -> GeoFile|None:
     file_type = get_file_type(file_path)
-    if file_type == FileType.GEOPACKAGE:
-        return GPKGFile(file_path)
-    elif file_type == FileType.SHAPEFILE:
-        return SHPFile(file_path)
-    else:
-        raise ValueError(f"Unsupported file type: {file_type}")
+    file_transport: dict[FileType, GeoFile] = {
+        FileType.GEOPACKAGE: GPKGFile(file_path),
+        FileType.SHAPEFILE: SHPFile(file_path)
+    }
+    return file_transport.get(file_type)
 
+def filter_process(file: GeoFile) -> GeoFile:
+    try:
+        filter_by = input(f"¿Desea filtrar los datos? (s/n):\n")
+        if filter_by.lower() != "s":
+            return file
+        column = input(f"¿Cuál es el índice de la columna a filtrar? Las columnas disponibles son: {file.columns}\n")
+        while True:
+            try:
+                if column.isdigit() and 0 <= int(column) < len(file.columns):
+                    column = file.columns[int(column)]
+                    break
+                else:
+                    print("Índice de columna inválido. Intente nuevamente.")
+            except ColumnNotFoundError as e:
+                print(e.message)
+                print("Intente nuevamente.")
+        print(f"Columna seleccionada: {column}")
+        valid_filters = file.valids_to_filter_values(column)
+        equals_to = input(f"¿Cuál es el valor que debe tener la columna {(column)} para filtrar los datos?\nLas opciones disponibles son: {valid_filters}\n")
+        while True:
+            if equals_to in valid_filters:
+                break
+            else:
+                equals_to = input("Valor inválido. Intente nuevamente.")
+        print(f"Filtrando los datos por {column} igual a {equals_to} ...")
+        return file.filter_data(column, equals_to)
+    except KeyboardInterrupt:
+        print("Proceso de filtrado interrumpido por el usuario.")
+        return file
+        
+    
 def main():
     start_time = datetime.now()
     print("IluminaRA iniciado")
-    file_path = ask_for_input()
-    file = get_file(file_path)
+    try:
+        try:
+            while True:
+                file_path = ask_for_input()
+                file = get_file(file_path)
+                if file is None:
+                    raise ValueError(f"Archivo no soportado: {file_path}")
+                break
+        except ValueError as e:
+            print(e)
+            exit(1)
+        print("¿Donde desea guardar el archivo resultante? Ingrese la ruta a continuación.")
+        while True:
+            try:
+                output_dir = ask_for_output_dir()
+                global RESOULTS_PATH
+                RESOULTS_PATH = output_dir
+                break
+            except PathError as e:
+                print(e.message)
+                print("Intente nuevamente.")
+                
 
-    if LAYER is not None:
-        all_ligths = read_file(
-            filename=POINTS_PATH,
-            layer=LAYER,
+        if isinstance(file, GPKGFile):
+            layer = input(f"El archivo {file_path.name} contiene las siguientes capas: {file.layers}\nIngrese el índice de la capa a leer: \n")
+            file = file.set_layer(layer)
+        file = filter_process(file)
+        ligths = file.geodata
+        print(f"{ligths.shape[0]} Fuentes en total")
+        x_limits, y_limts = grid_range_from_geodf(
+            geo_df=ligths, 
+            margin=MARGIN_FROM_POINTS,
+            margin_fn=MARGIN_FN_DICT[MARGIN_FN]
             )
-    else:
-        all_ligths = read_file(
-            filename=POINTS_PATH,
+        grid = Grid(
+            *x_limits,
+            *y_limts,
+            x_points=N_GRID_POINTS,
+            y_points=N_GRID_POINTS
             )
-    ligths = all_ligths.copy()
-    if FILTER_BY is not None:
-        ligths = filter_geodf(
-            geodf=all_ligths,
-            column=FILTER_COLUMN,
-            equals_to=FILTER_BY
-        )
-    print(f"Fuentes leídas desde {POINTS_PATH.name}")
-    print(f"{ligths.shape[0]} Fuentes en total")
-    x_limits, y_limts = grid_range_from_geodf(
-        geo_df=ligths, 
-        margin=MARGIN_FROM_POINTS,
-        margin_fn=MARGIN_FN_DICT[MARGIN_FN]
-        )
-    grid = Grid(
-        *x_limits,
-        *y_limts,
-        x_points=N_GRID_POINTS,
-        y_points=N_GRID_POINTS
-        )
-    xv, yv = grid.values
-    print(f"""Grilla creada
-        {N_GRID_POINTS=}
-        {MARGIN_FROM_POINTS=}
-        {xv.shape=}""")
-    scene = Scene()
-    print("Calculando SQM ...")
-    sqm = build_sqm_defined_ligths(scene, grid, ligths)
-    points = array2points(xv, yv)
-    geo_df = points_to_geodf(points, sqm)
-    output_path = export(geo_df=geo_df)
-    print(f"Archivo exportado en {RESOULTS_PATH.name} como {output_path.name}")
-    end_time = datetime.now()
-    print(f"Proceso terminado en {(end_time-start_time).seconds} segundos.")
+        xv, yv = grid.values
+        print(f"""Grilla creada
+            {N_GRID_POINTS=}
+            {MARGIN_FROM_POINTS=}
+            {xv.shape=}""")
+        scene = Scene()
+        print("Calculando SQM ...")
+        sqm = build_sqm_defined_ligths(scene, grid, ligths)
+        points = array2points(xv, yv)
+        geo_df = points_to_geodf(points, sqm)
+        output_path = export(geo_df=geo_df)
+        print(f"Archivo exportado en {RESOULTS_PATH.name} como {output_path.name}")
+        end_time = datetime.now()
+        print(f"Proceso terminado en {(end_time-start_time).seconds} segundos.")
+    except KeyboardInterrupt:
+        print("Proceso interrumpoido por el usuario.")
+        exit()
 
 if __name__ == "__main__":
     main()
