c925807e9e886501c7aff34c114d8fc8631d5fa5
Richard Povea <richardpovea@gmail.com>
2026-03-12 11:26:02 -0300

    Crea archivo processing para procesos del modelo

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from model.model import Scene, Grid, get_skyglow
-from model.luminica import ModifiedLightSourceAlbersDuricoe
+from model.model import Scene, Grid
 from geo import (
     array2points, grid_range_from_geodf
 )
 from state import State, Event, machine, FileType, get_file_type
 from errors import ColumnNotFoundError, DirectoryNotFoundError
 from logger import setup_logger, get_logger
-from config import AttributeNames, GridConfig, SQMConfig, MARGIN_FN_DICT, get_grid_config, get_sqm_config
+from config import AttributeNames, GridConfig, MARGIN_FN_DICT, get_grid_config, get_sqm_config
+from processig import build_sqm_defined_ligths
 
 from typing import Self, Callable
 from pathlib import Path
@@ -14,7 +14,6 @@ from numpy import ndarray
 from datetime import datetime
 from geopandas import read_file, list_layers, GeoDataFrame
 from geopandas.array import GeometryArray
-from shapely import Point
 from functools import lru_cache
 
 def get_default_config() -> dict:
@@ -52,49 +51,6 @@ def export(geo_df: GeoDataFrame, output_path: Path) -> Path:
     return output_path
 
 
-def add_light_source(scene: Scene, x: float, y: float, light_flux: int):
-    light_source = ModifiedLightSourceAlbersDuricoe(x, y, light_flux)
-    scene.add_light_source(light_source)
-
-
-def get_sqm(scene: Scene, grid: Grid, sqm_config: SQMConfig):
-    skyglow_albers_duricoe = get_skyglow(scene, grid)
-    skyglow_albers_duricoe += sqm_config.natural_bg_skyglow
-    sqm = sqm_config.cd_2_sqm(skyglow_albers_duricoe)
-    sqm[sqm > sqm_config.background_sqm] = sqm_config.background_sqm
-    return sqm
-
-
-def build_sqm_one_ligth(
-        scene: Scene,
-        grid: Grid,
-        ligths: GeoDataFrame,
-        electric_power: int,
-        luminosity_efficiency: int,
-        sqm_config: SQMConfig):
-    light_flux = electric_power * luminosity_efficiency
-    for lum in ligths.geometry:
-        if not isinstance(lum, Point):
-            raise TypeError("No corresponde a un punto")
-        add_light_source(scene=scene, x=lum.x, y=lum.y, light_flux=light_flux)
-    sqm = get_sqm(scene, grid, sqm_config)
-    return sqm
-
-
-def build_sqm_defined_ligths(
-        scene: Scene,
-        grid: Grid,
-        ligths: GeoDataFrame,
-        names: AttributeNames,
-        sqm_config: SQMConfig):
-    log = get_logger()
-    points = ligths.geometry
-    log.info(f"Calculando SQM con {points.shape[0]} fuentes lumínicas")
-    light_flux = ligths[names.power] * ligths[names.eficiency]
-    for x, y, flux in zip(points.x, points.y, light_flux):
-        scene.add_light_source(ModifiedLightSourceAlbersDuricoe(x, y, flux))
-    sqm = get_sqm(scene=scene, grid=grid, sqm_config=sqm_config)
-    return sqm
 
 
 def filter_geodf(
---
+++ processig.py
@@ -0,0 +1,45 @@
+from model.model import Scene, Grid, get_skyglow
+from model.luminica import ModifiedLightSourceAlbersDuricoe
+from config import AttributeNames, SQMConfig
+
+from geopandas import GeoDataFrame
+from shapely import Point
+
+def add_light_source(scene: Scene, x: float, y: float, light_flux: int):
+    light_source = ModifiedLightSourceAlbersDuricoe(x, y, light_flux)
+    scene.add_light_source(light_source)
+
+def build_sqm_one_ligth(
+        scene: Scene,
+        grid: Grid,
+        ligths: GeoDataFrame,
+        electric_power: int,
+        luminosity_efficiency: int,
+        sqm_config: SQMConfig):
+    light_flux = electric_power * luminosity_efficiency
+    for lum in ligths.geometry:
+        if not isinstance(lum, Point):
+            raise TypeError("No corresponde a un punto")
+        add_light_source(scene=scene, x=lum.x, y=lum.y, light_flux=light_flux)
+    sqm = get_sqm(scene, grid, sqm_config)
+    return sqm
+
+def get_sqm(scene: Scene, grid: Grid, sqm_config: SQMConfig):
+    skyglow_albers_duricoe = get_skyglow(scene, grid)
+    skyglow_albers_duricoe += sqm_config.natural_bg_skyglow
+    sqm = sqm_config.cd_2_sqm(skyglow_albers_duricoe)
+    sqm[sqm > sqm_config.background_sqm] = sqm_config.background_sqm
+    return sqm
+
+def build_sqm_defined_ligths(
+        scene: Scene,
+        grid: Grid,
+        ligths: GeoDataFrame,
+        names: AttributeNames,
+        sqm_config: SQMConfig):
+    points = ligths.geometry
+    light_flux = ligths[names.power] * ligths[names.eficiency]
+    for x, y, flux in zip(points.x, points.y, light_flux):
+        scene.add_light_source(ModifiedLightSourceAlbersDuricoe(x, y, flux))
+    sqm = get_sqm(scene=scene, grid=grid, sqm_config=sqm_config)
+    return sqm
